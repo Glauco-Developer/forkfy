2f438f6a2ecdc9a126b66a9f994578ae39aa8fcd
Glauco Raupp Russo <glauco.developer@Glaucos-MacBook-Pro.local>
2019-05-25 10:35:18 +0100

    base for dom elements, search results

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,30 @@
 import Search from './models/Search';
-
+import * as searchView from './views/searchView'
+import { elements } from './views/base';
 // Global state
 const state = {};
 
 const controlSearch = async () => {
     // Get query from view
-    const query = 'pizza';
+    const query = searchView.getInput();
+    console.log(query)
 
     if(query) {
         // New search object and add to state
         state.search = new Search(query);
         
         // Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
         
         // Search for recipes
         await state.search.getResults();
 
         // Render results on UI
-        console.log(state.search.result)
+        searchView.renderResults(state.search.result);
     }
 }
-
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
